37939a733be36290796239888891c96ba7afa4fd
Fabio Didio <120029824+fabiopdidio@users.noreply.github.com>
2023-07-10 11:21:16 -0300

    Create mediaAritmetica.js

---
+++ aula1/mediaAritmetica.js
@@ -0,0 +1,10 @@
+/*
+Cálculo de média aritmética de 4 notas
+*/
+
+let nota1 = Number(prompt("Digite a primeira nota: "));
+let nota2 = Number(prompt("Digite a segunda nota: "));
+let nota3 = Number(prompt("Digite a terceira nota: "));
+let nota4 = Number(prompt("Digite a quarta nota: "));
+
+console.log((nota1 + nota2 + nota3 + nota4) / 4);
